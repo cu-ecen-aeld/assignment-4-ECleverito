da639335f102fd861421f462e9287fde86c05cb4
ECleverito <eclever12@gmail.com>
2022-10-02 07:34:19 -0600

    Making updates for aesdsocket daemon app

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,13 +5,14 @@
 #
 ##############################################################
 
-AESD_ASSIGNMENTS_VERSION = '8352c291ef2fbd423804d6141afa57cfa98ef4ce'
+AESD_ASSIGNMENTS_VERSION = '9a181e269b3d30ee33045c143d90e3778f59c4a2'
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-ECleverito.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
@@ -21,6 +22,9 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
+
 endef
 
 $(eval $(generic-package))
--- clean.sh
+++ clean.sh
@@ -8,13 +8,13 @@ cd ${BASE_DIR}
 if [ ! -d ./base_external ] && [ ! -d ./buildroot ]
 then
 	echo "ERROR: This clean.sh is in an unfamiliar directory. Exiting."
-	exit(-1)
+	exit -1
 fi
 
 if [ ! -e ${BASE_DIR}/buildroot/Makefile ]
 then
 	echo "ERROR: No Makefile is present"
-	exit(-1)
+	exit -1
 fi
 
 cd ${BASE_DIR}/buildroot
